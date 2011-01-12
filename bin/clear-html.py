e82d34cbbe94efdd29c39dfd6b077bb7ff2e62eb
Lorenzo Bolla <lbolla@geneity.co.uk>
2011-01-12 13:50:31 +0000

    more html cleanup

--- bin/clear-html.py
+++ bin/clear-html.py
@@ -2,9 +2,40 @@
 
 import os
 import sys
+import re
+
 from lxml import etree as ET
 
-print sys.argv
+
+CLEAN_TEXT_RE = [
+		(re.compile('\t+'), ''),
+		(re.compile(' +'), ' '),
+		(re.compile('\r'), ''),
+		(re.compile('\n ?(\n *)+'), '\n\n'),
+		(re.compile('\n$'), ''),
+		]
+
+
+def clean_text(text):
+	for r, s in CLEAN_TEXT_RE:
+		text = r.sub(s, text)
+	return text
+
+
+def clean_doc(doc):
+
+	content_type = ET.Element('meta')
+	content_type.set('http-equiv', 'Content-Type')
+	content_type.set('content', 'text/html;charset=utf-8')
+	doc.find('head').append(content_type)
+
+	diego = ET.Element('span')
+	diego.text = 'Diego Fusaro'
+	for i in [img for img in doc.findall('.//img') if img.get('src') == 'DiegoLogoAnim2.gif']:
+		i.getparent().replace(i, diego)
+
+	return doc
+
 
 infile   = sys.argv[1]
 htmlfile = infile.split('.')[0] + '.out.html'
@@ -12,18 +43,12 @@ textfile = infile.split('.')[0] + '.out.txt'
 tmpfile  = '/tmp/tmp.html'
 
 doctype = '<!DOCTYPE HTML>'
-content_type = ET.Element('meta')
-content_type.set('http-equiv', 'Content-Type')
-content_type.set('content', 'text/html;charset=utf-8')
 
-doc = ET.HTML(open(infile, 'r').read())
-
-# FIXME: remove all the awful inline CSS
-doc.find('head').append(content_type)
+doc = clean_doc(ET.HTML(open(infile, 'r').read()))
 html = doctype + ET.tostring(doc, method='html', encoding='utf8')
-
-text = ET.tostring(doc, method='text', encoding='utf8')
-open(textfile, 'w').write(text)
-
 open(tmpfile, 'w').write(html)
 os.system('tidy -i -clean -utf8 -ashtml -w 0 -o %s %s' % (htmlfile, tmpfile))
+
+text = clean_text(ET.tostring(doc, method='text', encoding='utf8'))
+open(textfile, 'w').write(text)
+
