2f5f8cfa468e34a9c318dfd986bfdfaad86418c8
Lorenzo Bolla <lbolla@geneity.co.uk>
2011-01-12 17:37:41 +0000

    more html cleanups

--- bin/clear-html.py
+++ bin/clear-html.py
@@ -7,6 +7,10 @@ import re
 from lxml import etree as ET
 
 
+CLEAN_HTML_RE = [
+		(re.compile('&nbsp;'), ''),
+		]
+
 CLEAN_TEXT_RE = [
 		(re.compile('\t+'), ''),
 		(re.compile(' +'), ' '),
@@ -22,12 +26,25 @@ def clean_text(text):
 	return text
 
 
+def clean_html(text):
+	for r, s in CLEAN_HTML_RE:
+		text = r.sub(s, text)
+	return text
+
+
 def clean_doc(doc):
 
-	content_type = ET.Element('meta')
-	content_type.set('http-equiv', 'Content-Type')
-	content_type.set('content', 'text/html;charset=utf-8')
-	doc.find('head').append(content_type)
+	for item in doc.findall('.//meta') + \
+			    doc.findall('.//style') + \
+				doc.findall('.//content'):
+		item.getparent().remove(item)
+
+	head = doc.find('head')
+	if head is not None:
+		content_type = ET.Element('meta')
+		content_type.set('http-equiv', 'Content-Type')
+		content_type.set('content', 'text/html;charset=utf-8')
+		head.append(content_type)
 
 	diego = ET.Element('span')
 	diego.text = 'Diego Fusaro'
@@ -40,15 +57,14 @@ def clean_doc(doc):
 infile   = sys.argv[1]
 htmlfile = infile.split('.')[0] + '.out.html'
 textfile = infile.split('.')[0] + '.out.txt'
+errfile  = infile.split('.')[0] + '.out.err'
 tmpfile  = '/tmp/tmp.html'
 
+doc = clean_doc(ET.HTML(clean_html(open(infile, 'r').read())))
 doctype = '<!DOCTYPE HTML>'
-
-doc = clean_doc(ET.HTML(open(infile, 'r').read()))
 html = doctype + ET.tostring(doc, method='html', encoding='utf8')
 open(tmpfile, 'w').write(html)
-os.system('tidy -i -clean -utf8 -ashtml -w 0 -o %s %s' % (htmlfile, tmpfile))
+os.system('tidy -i -clean -utf8 -ashtml -f %s -w 0 -o %s %s' % (errfile, htmlfile, tmpfile))
 
 text = clean_text(ET.tostring(doc, method='text', encoding='utf8'))
 open(textfile, 'w').write(text)
-
