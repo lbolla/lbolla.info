0546e9b33d45b9be84b028252bc4856bb6e20eee
Lorenzo Bolla <lbolla@geneity.co.uk>
2011-01-12 12:07:10 +0000

    correctly handle utf8

--- bin/clear-html.py
+++ bin/clear-html.py
@@ -6,9 +6,10 @@ from lxml import etree as ET
 
 print sys.argv
 
-infile = sys.argv[1]
-outfile = sys.argv[2]
-tmpfile = '/tmp/tmp.html'
+infile   = sys.argv[1]
+htmlfile = infile.split('.')[0] + '.out.html'
+textfile = infile.split('.')[0] + '.out.txt'
+tmpfile  = '/tmp/tmp.html'
 
 doctype = '<!DOCTYPE HTML>'
 content_type = ET.Element('meta')
@@ -16,10 +17,13 @@ content_type.set('http-equiv', 'Content-Type')
 content_type.set('content', 'text/html;charset=utf-8')
 
 doc = ET.HTML(open(infile, 'r').read())
-doc.find('head').append(content_type)
-# FIXME: remove all the awful inline CSS
 
-html = doctype + ET.tostring(doc, method='html')
+# FIXME: remove all the awful inline CSS
+doc.find('head').append(content_type)
+html = doctype + ET.tostring(doc, method='html', encoding='utf8')
+
+text = ET.tostring(doc, method='text', encoding='utf8')
+open(textfile, 'w').write(text)
 
 open(tmpfile, 'w').write(html)
-os.system('tidy -i -clean -ashtml -w 0 -o %s %s' % (outfile, tmpfile))
+os.system('tidy -i -clean -utf8 -ashtml -w 0 -o %s %s' % (htmlfile, tmpfile))
