5965d68cf38001444084b2abb1999188d2a99f17
Lorenzo Bolla <lbolla@gmail.com>
2011-01-15 11:53:38 +0000

    optimization

--- screens/static/js/default.js
+++ screens/static/js/default.js
@@ -1,5 +1,8 @@
 function init () {
-	$('html').mousemove(function () { $('.fade').fadeIn(); });
+	$('html').mousemove(function () { 
+		$('.fade').fadeIn(); 
+		$(this).unbind('mousemove');
+	});
 }
 
 $(document).ready(init);
