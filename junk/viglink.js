7111abcee38c7a303099fe6a6983b695661cabed
Lorenzo Bolla <lorenzo@awesome.org>
2011-04-13 14:43:20 +0100

    remove useless code

--- junk/viglink.js
+++
@@ -1,9 +0,0 @@
-var vglnk = { api_url: '//api.viglink.com/api',
-key: '8675dff6abe627b9690aafbd6c605595' };
-
-(function(d, t) {
-    var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
-    s.src = ('https:' == document.location.protocol ? vglnk.api_url :
-        '//cdn.viglink.com/api') + '/vglnk.js';
-        var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
-}(document, 'script'));
